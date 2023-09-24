96b1cadac35ca9d21ea6bc1b878eea088f343b9f
sameerpatill <sameerspatil29@gmail.com>
2023-09-24 19:49:02 +0530

    Committed: 24-09-2023

---
+++ Strings/largestOddNumber.cpp
@@ -0,0 +1,30 @@
+//LeetCode
+//1903. Largest Odd Number in String
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        string ans = "";
+        int index = -1;
+
+        for(int i = n-1; i >= 0; i--)
+        {
+            if(num[i] % 2 != 0)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if(index != -1)
+        {
+            for(int i = 0; i <= index; i++)
+            {
+                ans += num[i];
+            }
+        }
+        return ans;
+    }
+   
+};
